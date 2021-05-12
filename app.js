c6d25e17e2c9429fd6ed4396627e3ad282656710
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-11 19:38:58 -0500

    Added routes and book model

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/React_Library', {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 
 app.get('/', (req, res)=> {
